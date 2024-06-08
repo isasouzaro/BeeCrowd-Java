a6956e36c53e9f105fe4c1c614b088875c986e90
Isa <isaisabela2008@gmail.com>
2024-06-07 22:07:16 -0300

    atualização var e metodos

---
+++ java-anatomia-classes/src/Aula3.java
@@ -0,0 +1,16 @@
+public class Aula3 {
+// Aprendendo a criar váriaveis e métodos 
+public static void main (String [] args) {
+    String primeiroNome = "Isabela";
+    String segundoNome = "Souza";
+
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+    System.out.println(nomeCompleto);
+
+}
+
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
+}
+}

962df773ecd7bcf000f4381b19d832501289776a
Isa <isaisabela2008@gmail.com>
2024-06-07 22:10:06 -0300

    inserindo nome

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -2,7 +2,8 @@ public class MinhaClasse {
     
 public static void main (String [] args) {
     
-    System.out.print ("Hello World!");
+    System.out.println ("Hello World!");
+    System.out.println("Isa");
 
 }
 
